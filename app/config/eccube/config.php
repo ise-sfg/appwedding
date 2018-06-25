24efbd65e9b0ef31947c37c202e75d699394d836
ise-masuyama <masuyama@iseise.co.jp>
2018-06-25 12:03:00 +0900

    plugin test13

--- app/config/eccube/config.php
+++ app/config/eccube/config.php
@@ -3,7 +3,7 @@ return [
     'auth_magic' => getenv("AUTH_MAGIC"),
     'password_hash_algos' => 'sha256',
     'shop_name' => getenv("SHOP_NAME"),
-    'force_ssl' => '1',
+    'force_ssl' => getenv("FORCE_SSL"),
     'cookie_lifetime' => '0',
     'locale' => 'ja',
     'timezone' => 'Asia/Tokyo',
---
+++ ss.sh
@@ -0,0 +1,5 @@
+export DATABASE_URL=postgres://postgres:iseise@localhost:5432/postgres
+export FORCE_SSL=0
+php -S 0.0.0.0:8000 -t html
+
+
